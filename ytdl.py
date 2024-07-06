90f3750ada8536c631884a123d1600938fed3c95
foggycaligo <dkdlwkr16@gmail.com>
2024-07-06 18:13:14 +0900

    test

---
+++ ytdl.py
@@ -0,0 +1,27 @@
+import yt_dlp
+
+def down_audio(url):
+ ydlOpts={
+  'extract_audio':True,
+  #"merge_output_format": 'mp3',
+  
+ }
+ with yt_dlp.YoutubeDL(ydlOpts) as ydl:
+  ydl.download([url])
+
+
+def down_video(url):
+ ydlOpts={
+    "format": f'best[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    "merge_output_format": 'mp4',
+    "overwrites": True,
+ }
+ with yt_dlp.YoutubeDL(ydlOpts) as ydl:
+  ydl.download([url])
+
+
+
+def main():
+ down_audio(input())
+
+main()
