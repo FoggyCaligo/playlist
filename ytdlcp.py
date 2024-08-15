f47d53d6170e3e48a6bf42f0070a849ac420269b
foggycaligo <dkdlwkr16@gmail.com>
2024-08-15 19:09:07 +0900

    succeed
    (cherry picked from commit 693a881ccc75baa3949e5f4b8f35b9f2c92ee4a3)

---
+++ ytdlcp.py
@@ -0,0 +1,37 @@
+import yt_dlp
+
+def down_audio(url):
+ ydlOpts={
+  'format':'bestaudio[ext=m4a]/best[ext=mp3]/best',
+  'extract_audio': True,
+  'overwrites':True,
+  "merge_output_format": 'mp3',
+  
+ }
+ with yt_dlp.YoutubeDL(ydlOpts) as ydl:
+  ydl.download([url])
+
+
+def down_video(url):
+ ydlOpts={
+    "format": 'best[height<=1080][ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+    "merge_output_format": 'mp4',
+    "overwrites": True,
+ }
+ with yt_dlp.YoutubeDL(ydlOpts) as ydl:
+  ydl.download([url])
+
+
+
+def main():
+ print("오디오1/비디오2")
+ i = input()
+ print("url 입력:")
+ if i=="2":
+  down_video(input())
+ else:
+  down_audio(input())
+ 
+ #down_audio(input())
+
+main()
